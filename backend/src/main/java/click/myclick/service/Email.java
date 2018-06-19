b99b42834f49043650aee4b1204a05801c2cae60
SUdoW <luizeduardo2607@gmail.com>
2018-06-19 11:05:17 -0300

    ...

--- backend/src/main/java/click/myclick/service/Email.java
+++ backend/src/main/java/click/myclick/service/Email.java
@@ -6,7 +6,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class Email {

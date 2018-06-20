a24aaa812fdb0ffbc5c9ebe6c5dbc440e11f72a6
Bruno Carvalho <comedinhass@gmail.com>
2018-06-20 14:58:33 -0300

    Corrigido erro do cors

--- backend/src/main/java/click/myclick/config/CORSConfig.java
+++ backend/src/main/java/click/myclick/config/CORSConfig.java
@@ -9,7 +9,8 @@ public class CORSConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*");
+    registry.addMapping("/**").allowedOrigins("*")
+                              .allowedHeaders("Content-Type");
   }
 
 }

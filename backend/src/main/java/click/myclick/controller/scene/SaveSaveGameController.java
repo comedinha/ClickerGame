8206d87b068e201c4e67bdef95fd760620c030b3
SUdoW <luizeduardo2607@gmail.com>
2018-07-03 08:58:34 -0300

    Controller do saveGame quase pronto

--- backend/src/main/java/click/myclick/controller/scene/SaveSaveGameController.java
+++ backend/src/main/java/click/myclick/controller/scene/SaveSaveGameController.java
@@ -1,7 +1,6 @@
 package click.myclick.controller.scene;
 
-import click.myclick.service.dao.scene.SceneService;
-import click.myclick.service.scene.LoadScene;
+import click.myclick.service.dao.game.SaveGameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,20 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/loadScene")
-public class SaveSaveController {
+public class SaveSaveGameController {
     
-    private final SceneService sceneService;
-    private final LoadScene loadScene;
+    private final SaveGameService saveGameService;
 
     @Autowired
-    public SaveSaveController(final SceneService sceneService, final LoadScene loadScene) {
-        this.sceneService = sceneService;
-        this.loadScene = loadScene;
+    public SaveSaveGameController(final SaveGameService saveGameService) {
+        this.saveGameService = saveGameService;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> save(@RequestBody final String idScene, Authentication auth) {
 
-        return new ResponseEntity<>(loadScene.getScene(sceneService, idScene), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
